3057d763ff3766cc47ffcb4587e318ed4debc677
jamiekariuki <jamiekariuki18@gmail.com>
2025-12-02 22:35:50 +0300

    fix: change helm default cert

--- argocd/provider.tf
+++ argocd/provider.tf
@@ -23,9 +23,9 @@ provider "kubernetes" {
 # Helm provider
 provider "helm" {
   kubernetes ={
-    host                   = data.aws_eks_cluster.this.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.this.token
+    host                   = data.aws_eks_cluster.default.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.default.token
   }
 }
 
