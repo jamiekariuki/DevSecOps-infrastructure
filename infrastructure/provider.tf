167e032193fb7e193b59f92f83ae7be79ad01e00
jamiekariuki <jamiekariuki18@gmail.com>
2026-01-05 22:29:33 +0300

    fix: add cloud init

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -3,3 +3,18 @@ provider "aws" {
   allowed_account_ids = [var.aws_account_id]
 }
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3"
+    }
+  }
+}
+
+
