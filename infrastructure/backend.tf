7d085cc226382d8bdb95a65719a0c042d55c0bc3
jamiekariuki <jamiekariuki18@gmail.com>
2026-01-05 22:54:53 +0300

    fix: update s3 file name

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "466872246229-state"
+    bucket = "850502433430-app-dev"
     key = "networking/terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "terraform-locks"
